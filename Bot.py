96205b2680eba6053c8aafbee547202bf11e0def
Xinderius <152814927+Xinderius@users.noreply.github.com>
2024-07-10 17:57:33 -0500

    bot de grupo con benja y joshua

---
+++ Bot.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+import random
+import os
+import requests
+
+intents = discord.Intents.default()
+intents.message_content = True
+mensaje = None
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+listamensajes = [
+    '''Reducir al máximo las cosas que tiramos, como las bolsas de plástico; reutilizar todo 
+    aquello que es aprovechable, como el jersey del hermano mayor, y guardar el papel o
+      las latas para que se puedan reciclar.''',
+      '''
+Si reciclas, puedes reutilizar; si reutilizas, puedes reducir; 
+si reduces, generarás un impacto importante sobre el medio ambiente
+ y abrirás el camino seguro para que tus hijos y nietos puedan disfrutar de un 
+ planeta limpio.
+'''
+'''Las «tres R«, nombre con el que habitualmente se conoce
+ este circuito ecológico, tienen una dimensión cronológica 
+ que se desenvuelve a lo largo del proceso del consumo:
+   previo a la decisión de adquirir un producto, durante el uso de él 
+   y una vez culminada su faceta productiva en la sociedad.''']
+
+def mensajes():
+    global mensaje
+    mensaje = random.choice(listamensajes)
+
+@bot.command()
+async def rrr(ctx):
+    mensajes()
+    await ctx.send(mensaje)
+
+
+bot.run("MTI2MDcyNzMwMDI3MDMyOTg1Ng.GTUr4E.MCuVmYEM6nvwh9uVUsTBK1ANkayDo7kgo8hAn8")
